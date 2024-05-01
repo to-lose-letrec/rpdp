cbfc2828556a357517132f508e733c36e9982de3
x <oscar.vermeulen@hotmail.com>
2024-05-01 23:43:57 +0200

    fix Mitch

--- bin-intel/rpdp.sh
+++ bin-intel/rpdp.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 
-
 # EDIT THE LINE BELOW TO MATCH THE NAME OF YOUR PIDP-10, (adding .local)
-pidpremote="pidp10.local"
+pidpremote="pidp10c.local"
 # EDIT THE LINE BELOW TO MATCH THE NAME OF YOUR PIDP-10, (adding .local)
 piuser="pi"
 # ----------------------------------------------------------------------
@@ -12,44 +11,47 @@ piuser="pi"
 echo "remote connection to $pidpremote"
 
 if [ $# -eq 0 ]; then
-    echo "ssh into $pidpremote."
-    echo " This will fail if the PDP-10 simulation is not running, or the user name on the PiDP-10 is not pi"
-    ssh -t $piuser@pidp10.local 'pdp'
+    echo "ssh into $pidpremote. Assumes the PDP-10 simulation is indeed running."
+    echo "opening virtual screen terminal, Ctrl-A d to leave."
+    ssh -t $piuser@$pidpremote 'pdp'
 
 else
 	case $1 in
 		con)
-			nohup /opt/pidp10/bin-intel/sty -e telnet $pidpremote 1025 > /dev/null 2>&1 &
+			nohup /opt/rpdp/bin/sty -e telnet $pidpremote 1025 > /dev/null 2>&1 &
 			;;
 		telcon)
 			nohup lxterminal --command="telnet $pidpremote 1025" > /dev/null 2>&1 &
 			;;
 		vt52)
-			nohup /opt/pidp10/bin-intel/vt52 -B telnet $pidpremote 10018 > /dev/null 2>&1 &
+			nohup /opt/rpdp/bin/vt52 -B telnet $pidpremote 10018 > /dev/null 2>&1 &
 			;;
-		vt05)
-			nohup /opt/pidp10/bin-intel/vt05 -B telnet $pidpremote 10018 > /dev/null 2>&1 &
-			;;
+#		vt05)
+#			nohup /opt/rpdp/bin/vt05 -B telnet $pidpremote 10018 > /dev/null 2>&1 &
+#			;;
 		tvcon)
-			nohup /opt/pidp10/bin-intel/tvcon -2BS $pidpremote > /dev/null 2>&1 &
+			nohup /opt/rpdp/bin/tvcon -2BS $pidpremote > /dev/null 2>&1 &
 			;;
 		imlac)
             echo "ITS tip: don't forget to enter :tctyp oimlac after logging in"
-			cd /opt/pidp10/bin-intel
-			nohup /opt/pidp10/bin-intel/imlac /opt/pidp10/bin-intel/imlac.simh > /dev/null 2>&1 &
+			cd /opt/rpdp/bin
+			nohup /opt/rpdp/bin/imlac /opt/rpdp/bin/imlac.simh > /dev/null 2>&1 &
 			;;
 		tek)
-			nohup /opt/pidp10/bin-intel/tek4010 -b9600 telnet $pidpremote 10017 > /dev/null 2>&1 &
+			nohup /opt/rpdp/bin/tek4010 -b9600 telnet $pidpremote 10017 > /dev/null 2>&1 &
 			;;
 		dp3300)
-			nohup /opt/pidp10/bin-intel/dp3300 -a -B telnet $pidpremote 10020 > /dev/null 2>&1 &	
+			nohup /opt/rpdp/bin/dp3300 -a -B telnet $pidpremote 10020 > /dev/null 2>&1 &	
 			;;
 		*)
 			echo rpdp for remote PiDP-10. Options are: 
-			echo  [con telcon vt52 vt05 tvcon imlac tek dp3300 ]
+			echo  [con telcon vt52 tvcon imlac tek dp3300 ]
 			echo when run without options, 
 			echo  pdp brings you into simh - Ctrl-A d to leave
-			;;
+			echo
+            echo edit rpdp.sh to change hostname and username 
+            echo for your PiDP-10, or run install-rpdp script
+            ;;
 	esac
 fi
 
